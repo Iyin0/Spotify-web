982c5756e7e02401bb02c2fd876f42f034599f82
Iyin0 <p2iyinoluwa@gmail.com>
2022-11-17 13:38:00 +0100

    convet algorithm update

--- src/convert.js
+++ src/convert.js
@@ -1,24 +1,33 @@
 const convert = (value) => {
     let hours = Math.floor(value / 3600);
     let minutes = Math.floor((value - (hours * 3600)) / 60);
-    let seconds = value - (hours * 3600) - (minutes * 60);
+    let seconds = Math.floor(value - (hours * 3600) - (minutes * 60));
 
-    if(hours < 1) {
+    if (hours < 1) {
         hours = "";
     }
-    if(minutes < 1) {
-        minutes = "0:"
+
+    else if (hours > 0) {
+        hours = hours + ":"
     }
-    else {
+
+    if (minutes < 1) {
+        minutes = "00:"
+    }
+    else if (minutes < 10) {
+        minutes = "0" + minutes + ":"
+    }
+    else if (minutes > 9) {
         minutes = minutes + ":";
     }
-    if(seconds < 1) {
+
+    if (seconds < 1) {
         seconds = "00"
     }
-    else if(seconds < 10) {
+    else if (seconds < 10) {
         seconds = "0" + seconds;
     }
-    return(`${hours}${minutes}${seconds}`)
+    return (`${hours}${minutes}${seconds}`)
 }
 
 export default convert
